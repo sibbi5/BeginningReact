922b5053f39303e05384836f46829095e5130ce7
sjothishunmugam <sjothishunmugam@deakin.edu.au>
2020-11-05 18:06:34 +1100

    Using useState function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 // const element = <h1>Hello World</h1>;
+// ------------------------------------------------------------------------------------------------
+// const lakelist = ["Echo Lake", "Maud Lake", "Cascade Lake"]
 
-const lakelist = ["Echo Lake", "Maud Lake", "Cascade Lake"]
+// function App(props){
+//     return(
+//         <ul>
+//             {props.lakes.map(lake => (<li>{lake}</li>))}
+//         </ul>
+//     );
+// }
+// -----------------------------------------------------------------------------------
 
-function App(props){
+
+function App(){
+    const [status,setStatus] = useState("Open");
     return(
-        <ul>
-            {props.lakes.map(lake => (<li>{lake}</li>))}
-        </ul>
+        <div>
+            <h1> Status : {status}</h1>
+            <button onClick={() => setStatus("Closed")}>
+                Closed
+            </button>
+
+            <button onClick={() => setStatus("Open")}>
+                Open
+            </button>
+
+            <button onClick={() => setStatus("Back in 5 ")}>
+                Break
+            </button>
+        </div>
     );
 }
 
+
 ReactDOM.render(
-    <App lakes={lakelist} />, 
+    <App  />, 
     document.getElementById('root'));
